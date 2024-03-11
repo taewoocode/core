e364c31cfe98b6a0268676621a72676691267f16
taewoocode <qkrxodn6035@gmail.com>
2024-03-12 00:30:31 +0900

    make testcode

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,4 +1,27 @@
 package hello.core.member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+
+    @Test
+    void join() {
+
+        //given ~ 이런이런 환경이 주어지고
+        Member member = new Member( 1L, "MemberA", Grade.VIP );
+
+
+        //when ~ 이렇게 되었을 때
+        memberService.join( member );
+        Member findMember = memberService.findMember( 1L );
+
+
+        //then ~ 이렇게 된다. then에서는 검증의 역할을 한다.
+        Assertions.assertThat( member ).isEqualTo( findMember ); // 조인한 것이랑, 찾은 거랑 똑같은지 검증
+
+
+    }
 }
