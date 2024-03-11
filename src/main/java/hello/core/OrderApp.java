cd1c598ee82266ef4315c0fa7f7cf21ccead991f
taewoocode <qkrxodn6035@gmail.com>
2024-03-12 01:12:59 +0900

    make  OrderApp, OrderServiceTest

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,26 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member( memberId, "memberA", Grade.VIP ); //vip 회원 만들기
+        memberService.join( member ); // DB에 넣어두기 -> 그래야 주문해서 찾아서 쓸 수 있다.
+
+        Order order = orderService.createOrder( memberId, "itemA", 10000 );
+
+        System.out.println( "order = " + order.toString() );
+
+    }
+}
