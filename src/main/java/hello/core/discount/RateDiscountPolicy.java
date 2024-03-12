76ca3727312dcc9caaae17791f420592cdcb8b6b
taewoocode <qkrxodn6035@gmail.com>
2024-03-12 21:56:08 +0900

    make RateDiscountPolicy

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100; // %이기 때문에 100을 나눠준다.
+        } else {
+            return 0;
+        }
+    }
+}
