8410c2561fb350fd20a96b4e3564fa134c462e4f
taewoocode <qkrxodn6035@gmail.com>
2024-03-12 23:05:51 +0900

    인터페이스에만 의존하게끔 변경

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RateDiscountPolicyTest {
@@ -20,7 +21,7 @@ class RateDiscountPolicyTest {
         //when
         int discount = discountPolicy.discount( member, 10000 );
         //then
-        Assertions.assertThat( discount ).isEqualTo( 1000 );
+        assertThat( discount ).isEqualTo( 1000 );
     }
 
     @Test
@@ -32,7 +33,7 @@ class RateDiscountPolicyTest {
         //when
         int discount = discountPolicy.discount( member, 10000 );
         //then
-        Assertions.assertThat( discount ).isEqualTo( 1000 );
+        assertThat( discount ).isEqualTo( 1000 );
     }
 }
 
