30e492ab89e106013954de7a5c69023954beda3c
taewoocode <qkrxodn6035@gmail.com>
2024-03-12 22:02:45 +0900

    RateDiscountPolicy

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -8,7 +8,7 @@ public class RateDiscountPolicy implements DiscountPolicy{
     private int discountPercent = 10;
 
     @Override
-    public int discount(Member member, int price) {
+    public int discount(Member member, int price) { // cmd + shift + t 테스트 코드 만들기
         if (member.getGrade() == Grade.VIP) {
             return price * discountPercent / 100; // %이기 때문에 100을 나눠준다.
         } else {
