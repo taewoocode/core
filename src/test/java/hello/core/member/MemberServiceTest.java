9a62dd9c46a6cfafef7b6e155a90a4a3f4ff7c64
taewoocode <qkrxodn6035@gmail.com>
2024-03-13 00:13:01 +0900

    make testcode

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,18 @@
 package hello.core.member;
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
 
+    @BeforeEach //test 실행 전 무조건 실행이 된다.
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig(); // 만들고
+        memberService = appConfig.memberService(); // 할당하고
+    }
 
     @Test
     void join() {
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,16 +1,25 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig(); // 만들고
+        memberService = appConfig.memberService();// 할당하고
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
@@ -19,7 +28,7 @@ public class OrderServiceTest {
         memberService.join( member );
 
         Order order = orderService.createOrder( memberId, "itemA", 1000 );
-        Assertions.assertThat( order.getDiscountPrice()).isEqualTo( 1000 );
+        Assertions.assertThat( order.getDiscountPrice()).isEqualTo( 1000);
 
     }
 }
