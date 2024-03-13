9883c1c6685d887221e6c9a35bac091831b162d6
taewoocode <qkrxodn6035@gmail.com>
2024-03-14 00:24:15 +0900

    Appconfig리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,13 +11,21 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); //생성자 주입
+        return new MemberServiceImpl( memberRepository() ); //생성자 주입
         //appConfig객체는 memoryMemberRepository객체를 생성하고 그 참조값을 memberServiceImpl을 생성하면서 생성자로 전달한다.
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl( new MemoryMemberRepository(), new FixDiscountPolicy()); //생성자 주입
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
+    public OrderService orderService() {
+        return new OrderServiceImpl( memberRepository(), discountPolicy()); //생성자 주입
+
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
