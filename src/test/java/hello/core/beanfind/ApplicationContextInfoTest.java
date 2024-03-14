1f494e7d2fdc85a59c14203493eb27d41fb84f54
taewoocode <qkrxodn6035@gmail.com>
2024-03-14 23:59:22 +0900

    빈 정보 역할 조회 기능 테스트

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -3,6 +3,7 @@ package hello.core.beanfind;
 import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class ApplicationContextInfoTest {
@@ -16,6 +17,23 @@ class ApplicationContextInfoTest {
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean( beanDefinitionName );
             System.out.println( "bean = " + beanDefinitionName + "obeject" + bean );
+        }
+
+
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames(); // 빈 정의된 이름 꺼내기
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition( beanDefinitionName );//metadata 정보 꺼내기
+
+            //
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean( beanDefinitionName );
+                System.out.println( "bean = " + beanDefinitionName + "obeject" + bean );
+            }
         }
 
 
