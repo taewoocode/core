66f07662a4c0fd8b6eed4465c1d284d477b71708
taewoocode <qkrxodn6035@gmail.com>
2024-03-17 17:44:50 +0900

    SingletonContainer Refac

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -31,4 +31,12 @@ public class ConfigurationSingletonTest {
         assertThat( memberService.getMemberRepository() ).isSameAs( memberRepository );
         assertThat( orderService.getMemberRepository() ).isSameAs( memberRepository );
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext( AppConfig.class ); //AppConfig도 스프링 빈으로 등록
+        AppConfig bean = ac.getBean( AppConfig.class );
+
+        System.out.println( "bean = " + bean.getClass() );
+    }
 }
